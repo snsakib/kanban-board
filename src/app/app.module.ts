86ee0365d4ff30ac91adc0d8941643d12b26e276
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-06-04 05:01:51 +0600

    feat: import DragDropModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NavbarComponent } from './navbar/navbar.component';
 import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 
@@ -16,7 +17,8 @@ import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
