6402ad4d1890ec2e9763fcfbac4fa0ed4bf82dca
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-06-04 04:43:11 +0600

    feat: generate kanban-board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './navbar/navbar.component';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    KanbanBoardComponent
   ],
   imports: [
     BrowserModule,
