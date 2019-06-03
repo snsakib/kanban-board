4dfddbf63fe8084029afefdff72214a5f88df6c5
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-06-04 04:57:53 +0600

    feat: add tasks data

--- src/app/kanban-board/kanban-board.component.ts
+++ src/app/kanban-board/kanban-board.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanban-board.component.scss']
 })
 export class KanbanBoardComponent implements OnInit {
+  todo = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5'];
+  inProgress = ['Task 6', 'Task 7', 'Task 8'];
+  done = ['Task 9', 'Task 10'];
 
   constructor() { }
 
