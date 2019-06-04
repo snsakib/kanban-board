371d9c8a8af00958d474fe38ad6a294c391df2c5
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-06-04 06:13:12 +0600

    feat: add drag & drop functionality

--- src/app/kanban-board/kanban-board.component.ts
+++ src/app/kanban-board/kanban-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban-board',
@@ -9,6 +10,14 @@ export class KanbanBoardComponent implements OnInit {
   todo = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5'];
   inProgress = ['Task 6', 'Task 7', 'Task 8'];
   done = ['Task 9', 'Task 10'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 
   constructor() { }
 
